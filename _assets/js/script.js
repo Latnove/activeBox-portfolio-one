454ea21f13faf42309c8dac5352dd4a5ccec0df5
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2021-11-06 16:29:38 +0300

    Исправлена навигация по сайту, созданы условия, отвечающие за меню-бургер

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,139 +1,93 @@
 const header = document.querySelector('.header')
-const navLinks = document.querySelectorAll('.navigation__link')
-const logoAnchor = document.querySelector('.logoAnchor')
+const headerLogo = document.querySelector('.header__body img')
 
-let posForLinks = new Array()
-let scrollTarget = null
-let activeNavLink = null
-let buttonPressed = false
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.header__link[data-goto]')
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick)
+  })
 
-window.addEventListener('resize', () => {
-  determinantOfCoordinates()
+  function onMenuLinkClick(e) {
+    const menuLink = e.target
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto)
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - (header.offsetHeight + 1)
+
+      if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock')
+        iconMenu.classList.remove('_active')
+        headerMenu.classList.remove('_active')
+        clickOnBurger = false // Так как бургер меню закрывается при переходе
+      }
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      })
+      e.preventDefault()
+    }
+  }
+}
+
+// Header Logo - при нажатии на него мы возвразаемся в САМЫЙ ВЕРХ
+headerLogo.addEventListener('click', function (zeroPosition) {
+  if (!header.classList.contains('_click-on-the-burger') || clickOnBurger === false) {
+    zeroPosition = 0
+
+    window.scrollTo({
+      top: zeroPosition,
+      behavior: 'smooth',
+    })
+  }
 })
-// Scroll
+
+// Menu Burger - его появление и исчезновение
+const iconMenu = document.querySelector('.header__burger')
+const headerMenu = document.querySelector('.header__menu')
+
+let deleteDelay // setTimeout
+let clickOnBurger = false
+
+if (iconMenu) {
+  iconMenu.addEventListener('click', () => {
+    if ((!header.classList.contains('_click-on-the-burger') && window.scrollY <= 0) || clickOnBurger === false) {
+      clickOnBurger = true
+      clearTimeout(deleteDelay)
+      header.classList.add('_click-on-the-burger')
+    } else if (clickOnBurger === true) {
+      clickOnBurger = false
+      removeBackgroundHeader()
+    }
+    console.log(clickOnBurger)
+
+    document.body.classList.toggle('_lock')
+    iconMenu.classList.toggle('_active')
+    headerMenu.classList.toggle('_active')
+  })
+}
+
+function removeBackgroundHeader() {
+  deleteDelay = setTimeout(() => {
+    header.classList.remove('_click-on-the-burger')
+  }, 500)
+}
+
+// Добавление фона для header при скролле
 window.addEventListener('scroll', scrollCheck)
 document.addEventListener('DOMContentLoaded', () => {
   scrollCheck()
-  determinantOfCoordinates()
 })
 
 function scrollCheck() {
   let scrollY = window.scrollY
 
   if (scrollY > 0) {
-    header.classList.add('headerScroll')
-    if (!logoAnchor.classList.contains('logoAnchor')) {
-      logoAnchor.classList.add('logoAnchor')
+    header.classList.add('_header-scroll')
+    if (!headerLogo.classList.contains('_header__logo')) {
+      headerLogo.classList.add('_header__logo')
     }
   } else {
-    header.classList.remove('headerScroll')
-    logoAnchor.classList.remove('logoAnchor')
-  }
-
-  // Navigation error
-  if (!buttonPressed) {
-    for (i = 0; i <= navLinks.length - 1; i++) {
-      if (scrollY >= posForLinks[i] && scrollY < posForLinks[i + 1]) {
-        const activeIndex = i
-        linkRemoveClass()
-        navLinks[activeIndex].classList.add('active')
-      } else if (scrollY < posForLinks[0]) {
-        linkRemoveClass()
-
-        break
-      }
-    }
-  } else if (buttonPressed) {
-    for (i = 0; i < posForLinks.length; i++) {
-      if (posForLinks[i] === scrollY) {
-        buttonPressed = false
-        break
-      }
-    }
-  }
-}
-
-// header Links error
-document.querySelectorAll('a.navigation__link').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault()
-
-    const href = this.getAttribute('href').substring(1)
-    winScrollY = window.scrollY
-
-    if (link != activeNavLink) {
-      activeNavLink = link
-      buttonPressed = true
-    } else if (link === activeNavLink) {
-      for (i = 0; i < posForLinks.length; i++) {
-        if (winScrollY === posForLinks[i]) {
-          buttonPressed = false
-
-          break
-        }
-      }
-    }
-
-    linkRemoveClass()
-    link.classList.add('active')
-
-    if (href === 'download') {
-      const scrollTarget = posForLinks[4]
-      window.scrollTo({
-        top: scrollTarget,
-        behavior: 'smooth',
-      })
-    } else {
-      scrollTarget = document.getElementById(href)
-
-      const topOffset = header.offsetHeight + 1
-      const elementPosition = scrollTarget.getBoundingClientRect().top //для определения позиции по top
-      const offsetPosition = elementPosition - topOffset
-
-      window.scrollBy({
-        top: offsetPosition,
-        behavior: 'smooth',
-      })
-    }
-  })
-})
-
-function linkRemoveClass() {
-  for (i = 0; i < navLinks.length; i++) {
-    navLinks[i].classList.remove('active')
-  }
-}
-
-// header Logo
-logoAnchor.addEventListener('click', function (zeroPosition) {
-  zeroPosition = 0
-
-  window.scrollTo({
-    top: zeroPosition,
-    behavior: 'smooth',
-  })
-})
-
-// Navigation error
-function determinantOfCoordinates() {
-  for (i = 0; i < navLinks.length + 1; i++) {
-    if (i === 5) {
-      posForLinks.push(document.body.scrollHeight)
-      // console.log(document.body.scrollHeight)
-      break
-    }
-    const href = navLinks[i].getAttribute('href')
-    if (href.substring(1) === 'download') {
-      const targetPos = document.body.clientHeight - window.innerHeight - 2
-      const lastScrollPos = targetPos
-      posForLinks.push(lastScrollPos)
-      // console.log(lastScrollPos)
-    } else {
-      const targetPos = document.querySelector(href).offsetTop
-      const scrollPos = targetPos - (header.offsetHeight + 1)
-      posForLinks.push(scrollPos)
-
-      // console.log(scrollPos)
-    }
+    header.classList.remove('_header-scroll')
+    headerLogo.classList.remove('_header__logo')
   }
 }
